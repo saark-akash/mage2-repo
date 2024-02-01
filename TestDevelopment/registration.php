3c2be9c0dd040f48a3bd811baa85b28ef9a1bf81
saark-akash <akash.rajput328@gmail.com>
2024-02-01 11:37:26 +0530

    mage price

---
+++ TestDevelopment/registration.php
@@ -0,0 +1,7 @@
+<?php
+// Akash Rajput
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Test_TestDevelopment',
+    __DIR__
+);
